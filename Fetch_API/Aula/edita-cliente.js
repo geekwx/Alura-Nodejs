54ac5aea4a7552f49718679e7af4390ac779ea8f
M.Iko <tagommm@gmail.com>
2020-11-13 15:41:18 -0300

    Aula 6

--- Fetch_API/Aula/edita-cliente.js
+++ Fetch_API/Aula/edita-cliente.js
@@ -11,6 +11,32 @@ detalhaCliente(id).then(dados => {
 })
 
 const formEdicao = document.querySelector('[data-form]')
+const mensagemSucesso = (mensagem) => {
+    const linha = document.createElement("tr");
+
+  const conteudoLinha = ` 
+      <div class="alert alert-success" role="alert"> 
+      ${mensagem}</div> 
+  `;
+
+  linha.innerHTML = conteudoLinha;
+
+  return linha;
+}
+const mensagemErro = (mensagem) => {
+    const linha = document.createElement("tr");
+
+  const conteudoLinha = ` 
+      <div class="alert alert-warning" role="alert"> 
+      ${mensagem}</div> 
+  `;
+
+  linha.innerHTML = conteudoLinha;
+
+  return linha;
+}
+
+
 
 formEdicao.addEventListener('submit', event => {
     event.preventDefault()
@@ -24,5 +50,10 @@ formEdicao.addEventListener('submit', event => {
         alert("ESSE CPF NÃO EXISTE")
         return 
     }
-    editaCliente(id, inputCPF.value, inputNome.value)
+    editaCliente(id, inputCPF.value, inputNome.value).then(resposta => {
+        if( resposta.status === 200){
+            formEdicao.appendChild(mensagemSucesso('Cliente editado com sucesso !'))
+        }else {formEdicao.appendChild(mensagemErro('Erro na edição do cliente'))
+        }
+    })
 })
