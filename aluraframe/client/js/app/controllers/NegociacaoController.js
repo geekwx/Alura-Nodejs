172660c2cbebb2524332303e6c3ada5453074ba3
GeekWx <geekwx@gmail.com>
2020-11-28 16:03:24 -0300

    Encerrando curso javascript  avançado

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -3,101 +3,46 @@ class NegociacaoController {
     constructor() {
         
         let $ = document.querySelector.bind(document);
+        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
-        this._listaNegociacoes = new Bind(new ListaNegociacoes(), new NegociacoesView($('#negociacoesView')),'adiciona', 'esvazia');
          
-
-        this._mensagem = new Bind(new Mensagem(),new MensagemView($('#mensagemView')), 'texto');
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(), 
+            new NegociacoesView($('#negociacoesView')), 
+            'adiciona', 'esvazia' , 'ordena', 'inverteOrdem');
+       
+        this._mensagem = new Bind(
+            new Mensagem(), new MensagemView($('#mensagemView')),
+            'texto');    
+            
+        this._ordemAtual = ''               
     }
     
     adiciona(event) {
-        
+       
         event.preventDefault();
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso'
-        this._limpaFormulario();   
+        try {
+            this._listaNegociacoes.adiciona(this._criaNegociacao());
+            this._mensagem.texto = 'Negociação adicionada com sucesso'; 
+            this._limpaFormulario();   
+        } catch(erro) {
+            this._mensagem.texto = erro;
+        }
     }
-    importaNegociacoes(){
-       
-      let service = new NegociacaoService();
-      service.obterNegociacoes()
-      .then(negociacoes => {
-          negociacoes.forEach(negociacao =>  this._listaNegociacoes.adiciona(negociacao));
-          this._mensagem.texto = 'Negociações do periodo importadas com sucesso';
-      })
-      .catch( erro => this._mensagem.texto = erro);
-/* 
-      Promise.all([
-          service.obterNegociacoesDaSemana(), 
-          service.obterNegociacoesDaSemanaAnterior(), 
-          service.obterNegociacoesDaSemanaRetrasada()]
-          ).then(negociacoes => {
-              negociacoes
-              .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
-              .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
-              this._mensagem.texto = 'Negociações importadas com sucesso';
-          })
-              .catch(error => this._mensagem.texto = error);
-*/
-      /*
-     service.obterNegociacoesDaSemana()
-      .then(negociacoes => {
-       negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
-       this._mensagem.texto = 'Negociações da semana obtida com sucesso';
-      })
-      .catch(erro => this._mensagem.texto = erro);
-      
+    
+    importaNegociacoes() {
+        
 
-      service.obterNegociacoesDaSemanaAnterior()
-      .then(negociacoes => {
-       negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
-       this._mensagem.texto = 'Negociações da semana obtida com sucesso';
-      })
-      .catch(erro => this._mensagem.texto = erro);
-     
-      service.obterNegociacoesDaSemanaRetrasada()
-      .then(negociacoes => {
-       negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
-       this._mensagem.texto = 'Negociações da semana obtida com sucesso';
-      })
-      .catch(erro => this._mensagem.texto = erro);
-     */
-       
-      /*
-      service.obterNegociacoesDaSemana((erro, negociacoes) => {
-        if(erro){
-            this._mensagem.texto = erro; 
-            return;
-        }
-        negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-        this._mensagem.texto = 'Negociações importadas com sucessos';  
-       });
-
-
-       service.obterNegociacoesDaSemanaAnterior((erro, negociacoes) => {
-        if(erro){
-            this._mensagem.texto = erro; 
-            return;
-        }
-        negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-        this._mensagem.texto = 'Negociações importadas com sucessos';  
-       });
-
-       service.obterNegociacoesDaSemanaRetrasada((erro, negociacoes) => {
-        if(erro){
-            this._mensagem.texto = erro; 
-            return;
-        }
-        negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-        this._mensagem.texto = 'Negociações importadas com sucessos';  
-       });*/
-
-
-
-
+        let service = new NegociacaoService();
+        service
+            .obterNegociacoes()
+            .then(negociacoes => negociacoes.forEach(negociacao => {
+                this._listaNegociacoes.adiciona(negociacao);
+                this._mensagem.texto = 'Negociações do período importadas'   
+            }))
+            .catch(erro => this._mensagem.texto = erro);                              
     }
     
     apaga() {
@@ -120,5 +65,15 @@ class NegociacaoController {
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
         this._inputData.focus();   
+    }
+    
+    ordena(coluna) {
+        
+        if(this._ordemAtual == coluna) {
+            this._listaNegociacoes.inverteOrdem(); 
+        } else {
+            this._listaNegociacoes.ordena((p, s) => p[coluna] - s[coluna]);    
+        }
+        this._ordemAtual = coluna;    
     }
 }
--- aluraframe/client/js/app/services/HttpService.js
+++ aluraframe/client/js/app/services/HttpService.js
@@ -1,54 +1,57 @@
 class HttpService {
+    
     get(url) {
-
+        
         return new Promise((resolve, reject) => {
-
+            
 
             let xhr = new XMLHttpRequest();
-
+            
             xhr.open('GET', url);
-    
+            
             xhr.onreadystatechange = () => {
-                  /*
-            Estado das requisições. 
-            0: Requisição ainda não iniciada
-            1: conexão com o servidor estabelecida
-            2: requisicao recebida
-            3: processando requisição
-            4: requisição concluida e a resposta pronta 
-            */
-              
-    
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-    
-                        console.log('Obtendo as negociaões do servidor.');
-                      resolve (JSON.parse(xhr.responseText)); 
-
-                    }else {
-                        reject(xhr.responseText);
                     
-                    }
-                }
-            }
-            xhr.send();
-        })
-    }
-    post (url, dado){
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState ==4){
-                    if (xhr.status == 200){
-                        resolve(JSON.parse(xhr.responseText));
-                    }else{
+                if(xhr.readyState == 4) {
+                    
+                    if(xhr.status == 200) {   
+                        
+                        resolve(JSON.parse(xhr.responseText));  
+                    } else {
+                        
                         reject(xhr.responseText);
                     }
                 }
             };
-            xhr.send(JSON.stringify(dado));
+            
+            xhr.send();
+             
+            
         });
     }
+    
+    post(url, dado) {
+
+
+            return new Promise((resolve, reject) => {
+
+                let xhr = new XMLHttpRequest();
+                xhr.open("POST", url, true);
+                xhr.setRequestHeader("Content-type", "application/json");
+                xhr.onreadystatechange = () => {
+
+                    if (xhr.readyState == 4) {
+
+                        if (xhr.status == 200) {
+
+                            resolve(JSON.parse(xhr.responseText));
+                        } else {
+
+                            reject(xhr.responseText);
+                        }
+                    }
+                };
+                xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
+            });
+
+        }
 }
