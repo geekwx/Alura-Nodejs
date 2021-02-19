df9f5f47a88775038de71e9d465a579453e42950
GeekWx <geekwx@gmail.com>
2021-02-19 05:55:55 -0300

    aula 4

--- 1-casadocodigo/casadocodigo/src/app/rotas/rotas.js
+++ 1-casadocodigo/casadocodigo/src/app/rotas/rotas.js
@@ -52,5 +52,13 @@ app.get('/livros', function(req, resp) {
     // );
     //     });
 });
+app.delete('/livros/:id', function(req, resp){
+    const id = req.params.id;
+
+    const livroDao = new LivroDao(db);
+    livroDao.remove(id)
+    .then(() => resp.status(200).end())
+    .catch(erro => console.log(erro));
+});
 
 }
