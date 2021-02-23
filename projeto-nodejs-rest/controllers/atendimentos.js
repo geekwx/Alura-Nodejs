1d7e53f4a6ee88b06c4ab5678f7e24d96ba16c03
Wesley Roberto <suporte@ubbonline.org.br>
2021-02-23 17:25:36 -0300

    adicionando banco

--- projeto-nodejs-rest/controllers/atendimentos.js
+++ projeto-nodejs-rest/controllers/atendimentos.js
@@ -3,4 +3,9 @@
 module.exports = app => {
 
     app.get('/atendimentos', (req, res) => res.send('Você esta na rota de atendimentos'));
+
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('Você esta na rota de atendimentos e de post')});
 }
---
+++ projeto-nodejs-rest/subirContainerMySQL.sh
@@ -0,0 +1,1 @@
+docker run --name teste-mysql -e MYSQL_ROOT_PASSWORD=admin -v /home/suporte/X/bancoDado/mysql:/var/lib/mysql -d mysql:5.7
