67fe229f1210ea2830f931aefadbcfb6f1ac47cf
Wesley Roberto <suporte@ubbonline.org.br>
2021-02-24 17:34:50 -0300

    3.1

--- projeto-nodejs-rest/controllers/atendimentos.js
+++ projeto-nodejs-rest/controllers/atendimentos.js
@@ -1,11 +1,12 @@
-
+const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-
-    app.get('/atendimentos', (req, res) => res.send('Você esta na rota de atendimentos'));
-
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
 
     app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
         console.log(req.body)
-        res.send('Você esta na rota de atendimentos e de post')});
+        Atendimento.adiciona(atendimento)
+        res.send('Post atendimento')
+    })
 }
---
+++ projeto-nodejs-rest/outro.md
@@ -0,0 +1,5 @@
+# Banco Mysql 
+
+**Alteração de tabela** 
+
+ALTER TABLE atendimentos ADD data datetime NOT NULL, ADD dataCriacao datetime NOT NULL;
