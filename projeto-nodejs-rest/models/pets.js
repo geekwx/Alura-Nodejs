5ab673d88b9115813410b71db43fc1887dfc5fbc
Wesley Roberto <suporte@ubbonline.org.br>
2021-02-26 17:14:33 -0300

    04.04

--- projeto-nodejs-rest/models/pets.js
+++ projeto-nodejs-rest/models/pets.js
@@ -1,18 +1,29 @@
 const conexao = require('../infraestrutura/conexao');
-
+const uploadDeArquivo = require('../arquivos/uploadDeArquivos')
 
 class Pet {
-    adiciona(pet){
+    adiciona(pet, res){
         const query = 'INSERT INTO Pets SET ?'
-        conexao.query(query, pet,  erro => {
+
+        uploadDeArquivo(pet.imagem, pet.nome, (erro, novoCaminho) => {
+
             if(erro){
-                console.log(erro)
-                resizeBy.status(400).json(erro)
-            }else {
-                resizeBy.status(200).json(pet)
+                res.status(400).json({erro})
+            } else {
+                const novoPet = { nome: pet.nome, imagem: novoCaminho}
+                conexao.query(query, novoPet,  erro => {
+                    if(erro){
+                        console.log(erro)
+                        res.status(400).json(erro)
+                    }else {
+                        res.status(200).json(novoPet)
+                    }
+                })
             }
+           
         })
+        
     }
 }
 
-module.exports = new Pet()
+module.exports = new Pet() 
